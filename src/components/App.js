4c431784fa15aee554b9b8cd401f9b17a8bd38c9
Anna Wang <38119775+annalinwang@users.noreply.github.com>
2021-03-02 15:17:56 -0500

    uh
    idk

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component, useState} from 'react'
+
+const App = () => {
+      const [posts, setPosts] = useState('')
+      changeVote = () => {
+      //... makes a copy of that value
+      const postsCopy = [...posts, newPosts]
+      //access posts through indexing
+      newPosts[0].vote = asldkfj
+      setsPosts(newPosts)
+    }
+  }
+
+export default App
+  
